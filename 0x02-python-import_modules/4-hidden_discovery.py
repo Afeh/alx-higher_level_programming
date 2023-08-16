0129d16495f7ce204a85365285df4725bffd6b9d
Afeh <balogunafebu22@gmail.com>
2023-08-16 16:16:04 +0100

    Code correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+import hidden_4
+
+def finder():
+    for k in dir(hidden_4):
+        if not (k[0] == '_' and k[1] == '_'):
+            print(k)
+
 
 if __name__ == "__main__":
-    hidden = __import__("hidden_4")
-    print(*[name for name in dir(hidden) if not name.startswith("__")], sep="\n")
+    finder()
