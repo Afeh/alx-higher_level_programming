44817d3f75f20f9fdd1e5b7890ad657dee1e77fc
Afeh <balogunafebu22@gmail.com>
2023-08-16 16:06:22 +0100

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import hidden_4
+
+def main():
+    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    print(*module_names, sep="\n")
+
 if __name__ == "__main__":
-    hidden_names = [name for name in dir(__import__("hidden_4")) if not name.startswith("__")]
-    print(*hidden_names, sep="\n")
+    main()
