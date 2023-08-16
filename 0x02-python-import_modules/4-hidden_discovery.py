d912bf710b83ebd03c5394283fe1436d46bb5d4f
Afeh <balogunafebu22@gmail.com>
2023-08-16 16:07:36 +0100

    Code correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-import hidden_4
-
-def main():
-    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    print(*module_names, sep="\n")
-
 if __name__ == "__main__":
+    hidden = __import__("hidden_4")
+    print(*[name for name in dir(hidden) if not name.startswith("__")], sep="\n")
     main()
