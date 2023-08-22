6a5be646b8cb366eee4912b0971aed69683d6041
Afeh <balogunafebu22@gmail.com>
2023-08-22 10:55:48 +0100

    Code correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,27 +6,23 @@ def main(argv):
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
-
     if operator == '+':
-        result = add(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
     elif operator == '-':
-        result = sub(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
     elif operator == '*':
-        result = mul(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
     elif operator == '/':
-        if b == 0:
-            print("Error: Division by zero")
-            exit(1)
-        result = div(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit(1)
 
-    print(f"{a} {operator} {b} = {result}")
+
+
 
 if __name__ == "__main__":
     import sys
