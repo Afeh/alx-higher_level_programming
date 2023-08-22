9444fdf1bb9d688c4f2651360dd979cea65be9e0
Afeh <balogunafebu22@gmail.com>
2023-08-22 10:33:46 +0100

    lint correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def finder():
     dir_name = dir(hidden_4)
     for k in dir_name:
         if k[:2] != '__':
             print("{:s}".format(k))
-
 
 if __name__ == "__main__":
     finder()
